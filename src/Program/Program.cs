ee55c5b51ea6826df00bbed2634ae643be4e19e9
Ezequiel Gomez <ezecam2019@gmail.com>
2021-09-08 18:12:29 -0300

    fix

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,15 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            FileType file = new FileType("\n", "../../assets/board.txt");
+            bool[,] boardBool = ReadFile.Read(file);
+            Board board = new Board(boardBool);
+            while (true)
+            {
+                Logic.Game(board);
+                PrintBoard.Print(board);
+            }
+            
         }
     }
 }
